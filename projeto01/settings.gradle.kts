d1ca3b5762439a942e2fddca6e3b1312ec19057c
jramosfilho27 <jramosfilho27@yahoo.com>
2023-05-04 21:22:23 -0300

    Arquivos iniciais do projeto01

---
+++ projeto01/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "tqi.jramos.projeto1"
---
+++ projeto01/src/main/kotlin/tqi/jramos/plugins/Routing.kt
@@ -0,0 +1,15 @@
+package tqi.jramos.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.http.content.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+
+    routing {
+        static("/static") {
+            resources("files")
+        }
+    }
+}
